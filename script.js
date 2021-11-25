26bf2c27c46ddee4f988bb4a8d56cb55d671efd6
ALEXANDRE SILVA DOS SANTOS <82949368+xandeprojetos@users.noreply.github.com>
2021-11-25 16:55:15 +0000

    Criação da segunda pagina

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
     var sidebar = document.querySelector(".container").classList.toggle("show-menu");
 });
+
+var btn = document.getElementById('btn-saibamais');
+        btn.addEventListener('click', function(e){
+      // funcão aqui
+          
+});    
+
